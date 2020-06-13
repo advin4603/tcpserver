13baf43ed3caf4c09847a656dc1b1bf64fea2ad5
Ayan-Home <advin4603@gmail.com>
2020-06-13 22:43:09 +0530

    Completed documentation for server.py

--- tcpsockets/server.py
+++ tcpsockets/server.py
@@ -1,9 +1,14 @@
+"""
+server.py
+    Provides classes to create servers and handle their clients.
+"""
+
 import socket
 from . import logger
 from .settings import default_header_size, default_chunk_size, default_port, default_queue
 from abc import ABC, abstractmethod
 import threading
-from typing import Tuple, Any, Callable, List
+from typing import Tuple, Any, Callable, List, Union
 import pickle
 import traceback
 
@@ -93,7 +98,7 @@ class Server(ABC):
 
     def stop_running(self) -> None:
         """
-        Calling this function stops the Server.
+        Calling this method stops the Server.
         Returns: None
         """
         self.running = False
@@ -186,15 +191,39 @@ class Client:
 
 
 class SequentialServer(Server):
+    """
+        A Sequential Server for handling clients one by one.
+
+        Args:
+            ip (str): The ip address(IPV4) of the server. Defaults to local machine's ip
+            port(int): The port the server must be bound to. Defaults to tcpsockets.settings.default_port if
+                       it is not set to None else raises Exception.
+            queue(int): The waiting queue length of the server. Defaults to tcpsockets.settings.default_queue
+                        if it is not set to None else raises Exception.
+            background(bool): Whether the server runs in background (in separate thread) or not. Defaults to True
+        Attributes:
+            handling(bool): A bool saying whether the server is handling a client or not.
+            stopper_thread(threading.Thread): A Thread that is responsible for stopping the Server.
+            current_client(Union[None, Client]): The Client Object of the client currently being handled.
+                                                 If not handling then it is set to None.
+        """
+
     def __init__(self, ip: str = socket.gethostbyname(socket.gethostname()), port: int = None, queue: int = None,
                  background: bool = True):
         super(SequentialServer, self).__init__(ip, port, queue, background)
-        self.handling = False
-        self.stopper_thread = threading.Thread(target=self.stopper)
+        self.handling: bool = False
+        self.stopper_thread: threading.Thread = threading.Thread(target=self.stopper)
 
-        self.current_client = None
+        self.current_client: Union[None, Client] = None
 
-    def stopper(self):
+    def stopper(self) -> None:
+        """
+        A stopper method running in its own thread to stop the server if it is stuck in the blocking socket.accept.
+        Creates a new socket to stop socket.accept.
+
+        Returns:
+            None
+        """
         while self.running or self.handling:
             pass
         closer_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -205,7 +234,12 @@ class SequentialServer(Server):
         logger.close_log_files()
         closer_socket.close()
 
-    def starter(self):
+    def starter(self) -> None:
+        """
+        The starter method is responsible for accepting connections from clients and passing them to the client_handler.
+        Returns:
+            None
+        """
         self.running = True
         self.stopper_thread.start()
         logger.log(f"Listening for connections on {self.ip} at {self.port}")
@@ -227,16 +261,43 @@ class SequentialServer(Server):
 
 
 class ParallelServer(Server):
+    """
+    A Parallel Server for Handling multiple clients simultaneously.
+    Args:
+            ip (str): The ip address(IPV4) of the server. Defaults to local machine's ip
+            port(int): The port the server must be bound to. Defaults to tcpsockets.settings.default_port if it is
+                       not set to None else raises Exception.
+            queue(int): The waiting queue length of the server. Defaults to tcpsockets.settings.default_queue if
+                        it is not set to None else raises Exception.
+            background(bool): Whether the server runs in background (in separate thread) or not. Defaults to True
+        Attributes:
+            client_threads(List[threading.Thread]): A list of all threads that have handled or are handling clients.
+            clients(List[Client]): List of all the Clients currently being handled.
+            stopper_thread(threading.Thread): A Thread that is responsible for stopping the Server.
+            server_thread(threading.Thread): If background is True. This attribute is the thread the server
+                                             is running on.
+
+    """
+
     def __init__(self, ip: str = socket.gethostbyname(socket.gethostname()), port: int = None, queue: int = None,
                  background: bool = True):
         super(ParallelServer, self).__init__(ip, port, queue, background)
         self.client_threads: List[threading.Thread] = []
         self.clients: List[Client] = []
-        self.stopper_thread = threading.Thread(target=self.stopper)
+        self.stopper_thread: threading.Thread = threading.Thread(target=self.stopper)
         if self.background:
-            self.server_thread = threading.Thread(target=self.starter)
+            self.server_thread: threading.Thread = threading.Thread(target=self.starter)
 
-    def client_func(self, client: Client):
+    def client_func(self, client: Client) -> None:
+        """
+        This method handles exceptions in the handler method and closes client socket after handling.
+        The client_thread is created from this function
+        Args:
+            client(Client): The client that needs to be handled.
+
+        Returns:
+            None
+        """
         try:
             self.handler(client)
         except BaseException:
@@ -250,6 +311,12 @@ class ParallelServer(Server):
                 return
 
     def starter(self):
+        """
+        The starter method is responsible for accepting connections from clients passing them to the client_handler and
+        creating a new thread.
+        Returns:
+            None
+                """
         self.running = True
         self.stopper_thread.start()
         logger.log(f"Listening for connections on {self.ip} at {self.port}")
@@ -268,10 +335,22 @@ class ParallelServer(Server):
         self.socket.close()
 
     @property
-    def handling(self):
+    def handling(self) -> bool:
+        """
+        This property returns whether the server is handling a client or not.
+        Returns:
+            bool: Whether the server is handling a client or not.
+        """
         return any([i.is_alive() for i in self.client_threads])
 
-    def stopper(self):
+    def stopper(self) -> None:
+        """
+        A stopper method running in its own thread to stop the server if it is stuck in the blocking socket.accept.
+        Creates a new socket to stop socket.accept.
+
+        Returns:
+            None
+        """
         while 1:
             if not self.running and not self.handling:
                 closer_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
